efaa2afb673c77537aabf48b8a7b744662ffd91d
sanathsb98 <itssanathsb@gmail.com>
2024-02-23 10:33:02 +0530

    signin route added

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const getAllUsers = require('../controller/user-controller').getAllUsers;
 const registerNewUser = require('../controller/user-controller').registerNewUser;
+const signin = require('../controller/user-controller').signin;
 
 const router = express.Router()
 
 router.get("/api/users",getAllUsers)
 router.post("/api/signup",registerNewUser)
-
+router.get("/api/signin",signin)
 
 module.exports = router;
