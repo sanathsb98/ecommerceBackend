d4e72e1f4e34796788883958a10383b78b77bed4
sanathsb98 <itssanathsb@gmail.com>
2024-02-27 14:34:11 +0530

    token time

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
 
-const generateToken = (user) => jwt.sign({id:user.id},process.env.SECRET_KEY,{expiresIn:'1d'})
+const generateToken = (user) => jwt.sign({id:user.id},process.env.SECRET_KEY,{expiresIn:'2m'})
 
 module.exports = generateToken
